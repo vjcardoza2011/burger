6aa08913e323fcb5e33fe42e2e75ee480d4dd912
Valarie Cardoza <vjcardoza2011@yahoo.com>
2019-06-15 18:23:48 -0500

    multiple changes, need fixes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,32 +1,32 @@
 var express = require('express');
-var eRouter = express.Router();
+var Router = express.Router();
 var burger = require('../models/burger.js');
 
 
 //ROUTES
 
-eRouter.get('/', function (req, res) {
+Router.get('/', function (req, res) {
     res.redirect('/index');
 });
 
-eRouter.get('/index', function (req, res) {
+Router.get('/index', function (req, res) {
     burger.selectAll(function (data) {
         var hbObject = { burgers: data };
         res.render('index', hbObject);
     });
 });
 
-eRouter.post('/burger/create', function (req, res) {
+Router.post('/burger/create', function (req, res) {
     burger.insertOne(req.body.burger_name, function () {
         res.redirect('/index');
     });
 });
 
-eRouter.post('/burger/eat/:id', function (req, res) {
+Router.post('/burger/eat/:id', function (req, res) {
     burger.updateOne(req.params.id, function () {
         res.redirect('/index');
     });
 });
 
 // Exporting routes
-module.exports = eRouter;
+module.exports = Router;
