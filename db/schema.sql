521fe845469992f3361ac3e799b3440ce6392e91
Valarie Cardoza <vjcardoza2011@yahoo.com>
2019-06-15 19:04:03 -0500

    fixed burger_name column

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE `burgers_db`;
 CREATE TABLE `burgers`
 (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`burger_name` VARCHAR(255) NOT NULL,
+	`burger_name` VARCHAR(255) NULL,
 	`devoured` BOOLEAN DEFAULT false,
 	`date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
