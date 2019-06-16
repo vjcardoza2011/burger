daa112de02fc2a01c9dc3ec30e536a12689d2f18
Valarie Cardoza <vjcardoza2011@yahoo.com>
2019-06-15 23:16:29 -0500

    trying a fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var methodOverride = require('method-override');
 var PORT = process.env.PORT || 3000;
 
 var app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + 'public'));
 
 // Parse application
 app.use(bodyParser.urlencoded({ extended: false }));
