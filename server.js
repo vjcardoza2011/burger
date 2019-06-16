b2609406ed84fb6463ef8075600d6fcdd47c07ad
Valarie Cardoza <vjcardoza2011@yahoo.com>
2019-06-15 23:42:55 -0500

    changes to server.js

--- server.js
+++ server.js
@@ -2,21 +2,25 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-
+var path = require('path')
+var app = express();
 
 // Open Server
 var PORT = process.env.PORT || 3000;
 
-var app = express();
-app.use(express.static(__dirname + 'public'));
+
+app.use(express.static(process.cwd() + '/public'));
 
 // Parse application
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Handlebars
+app.use(methodOverride('_method'))
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 //ROUTES
 var router = require('./controllers/burgers_controller.js');
